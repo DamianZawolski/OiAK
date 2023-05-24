ccf27451e84fed919073cdf6831ae4954e48e924
DamianZawolski <116562556+DamianZawolski@users.noreply.github.com>
2023-05-25 00:50:22 +0200

    Add files via upload

--- binary_operations.py
+++ binary_operations.py
@@ -1,71 +1,87 @@
+from math import log2, floor
+
 # zamiana liczby dziesiętnej na wektor binarny
 def decimal_to_binary(decimal_x):
     result = []
-    while decimal_x > 1:
-        result.insert(0, decimal_x % 2 == 1)
+    while decimal_x > 0:
+        result.insert(0, decimal_x % 2)
         decimal_x = decimal_x // 2
-    result.append(decimal_x)
-
+    
     return result
 
-
-def pad_with_zeros(x, total_length):
-    while len(x) < total_length:
-        x.insert(0, False)
-
-
-# mnożenie dwóch wektorów bitowych
-def binary_multiplication(v_a, v_b):
-    if len(v_a) < len(v_b):
-        pad_with_zeros(v_a, len(v_b))
-    elif len(v_b) < len(v_a):
-        pad_with_zeros(v_b, len(v_a))
-
-    result = []
-
-    # TODO
-
-    return result
+# Funkcja wypełniająca wektory bitowe zerami, aby miały taką samą długość równej długości wektora o największej długości
+def fill_with_zeros(binary_a, binary_b):
+    length_a = len(binary_a)
+    length_b = len(binary_b)
+    if length_a > length_b:
+        binary_b = [0] * (length_a - length_b) + binary_b
+    elif length_b > length_a:
+        binary_a = [0] * (length_b - length_a) + binary_a
+    return binary_a, binary_b
 
 
-def binary_addition(v_a, v_b):
-    if len(v_a) < len(v_b):
-        pad_with_zeros(v_a, len(v_b))
-    elif len(v_b) < len(v_a):
-        pad_with_zeros(v_b, len(v_a))
+# Funkcja porównująca dwa wektory bitowe (gdzie False oznacza 0, a Trye oznacza 1) i zwracająca True, jeśli pierwszy wektor jest większy lub równy drugiemu, a False w przeciwnym wypadku.
+def compare_binary(binary_a, binary_b):
+    # Sprawdzenie, który wektor jest dłuższy
+    length_a = len(binary_a)
+    length_b = len(binary_b)
+    if length_a > length_b:
+        binary_b = [0] * (length_a - length_b) + binary_b
+    elif length_b > length_a:
+        binary_a = [0] * (length_b - length_a) + binary_a
 
-    result = []
-    carry = False
-    for i in reversed(range(0, len(v_a))):
-        # 1 1
-        if v_a[i] & v_b[i]:
-            result.insert(0, False ^ carry)
-            carry = True
-        # 1 0 albo 0 1
-        elif v_a[i] | v_b[i]:
-            result.insert(0, True ^ carry)
-        # 0 0
-        else:
-            result.insert(0, 0 ^ carry)
-            carry = False
-
-    if carry:
-        result.insert(0, True)
-
-    return result
-
-
-# v_a > v_b
-def compare_binary(v_a, v_b):
-    if len(v_a) < len(v_b):
-        pad_with_zeros(v_a, len(v_b))
-    elif len(v_b) < len(v_a):
-        pad_with_zeros(v_b, len(v_a))
-
-    for i in range(len(v_a)):
-        if v_a[i] and not v_b[i]:
+    # Iteracja od końca wektorów
+    for bit_a, bit_b in zip(binary_a[::-1], binary_b[::-1]):
+        # Jeśli bit_a jest większy od bit_b, to zwróć True
+        if bit_a > bit_b:
             return True
-        elif v_b[i] and not v_a[i]:
+        # Jeśli bit_b jest większy od bit_a, to zwróć False
+        elif bit_b > bit_a:
             return False
 
-    return False
+    # Jeśli oba wektory są takie same, to zwróć True
+    return True
+
+# Funkcja konwertująca wektor bitowy na liczbę dziesiętną
+def binary_to_decimal(binary_x):
+    result = 0
+    for i in range(len(binary_x)):
+        result += binary_x[i] * 2 ** (len(binary_x) - i - 1)
+    return result
+
+# funkcja wykonująca operację X(mod P) na dwóch wektorach bitowych (gdzie X to binary_x, a P to binary_p)
+def modulo_function(X, P):
+    X, P = fill_with_zeros(X, P)
+    print("X = ", X)
+    print("P = ", P)
+    r = len(P)
+    k = len(X) // r
+    n = len(X)
+
+    if len(X) < k * r:
+        X = [0] * (k * r - n) + X
+
+    X_parts = [X[i*r:(i+1)*r] for i in range(k, 0, -1)]
+    X_parts.reverse()
+
+    S = sum([(int("".join(map(str, X_i)), 2) * (2 ** ((i+1) * (r-1)))) % int("".join(map(str, P)), 2) for i, X_i in enumerate(X_parts)])
+
+    while S > (2 * int("".join(map(str, P)), 2)):
+        n_temp = len(str(bin(S))) - 2
+        k_temp = n_temp // r
+
+        if len(str(bin(S))) - 2 < k_temp * r:
+            S = int(bin(S)[(k_temp * r) - n_temp:], 2)
+
+        S_parts = [str(bin(S))[i*r:(i+1)*r] for i in range(k_temp, 0, -1)]
+        S_parts.reverse()
+
+        S = sum([(int(S_i, 2) * (2 ** ((i+1) * (r-1)))) % int("".join(map(str, P)), 2) for i, S_i in enumerate(S_parts)])
+
+        if int("".join(map(str, P)), 2) <= S:
+            S -= int("".join(map(str, P)), 2)
+        else:
+            break
+
+    return S
+
--- main.py
+++ main.py
@@ -1,59 +1,17 @@
-from math import log2, floor
 from binary_operations import *
 
 def main():
-    x = 262149
-    p = 47
-    bits = decimal_to_binary(x)
-
-    modulo_function(bits, p)
-
-
-def split_into_subvectors(binary_x, r):
-    result = []
-    while binary_x:
-        result.append(binary_x[:r])
-        binary_x = binary_x[r+1:]
-
-    return result
-
-
-# funkcja wykonująca operację X(mod P)
-def modulo_function(bits, p):
-    r = floor(log2(p) - 1)
-
-    # dodanie zer na początek żeby utworzyć subwektory równej długości
-    while len(bits) % r != 0:
-        bits.insert(0, 0)
-
-    # podział wektora bitów na subwektory
-    subvectors = split_into_subvectors(bits, r)
-    k = len(subvectors)
-
-    x = []
-    for i in range(0, k):
-        subv = subvectors[i]
-        x = binary_addition(x, binary_multiplication(subv, pow(2, r*(i-1)) % p))
-
-    s = x
-    s_temp = s
-
-    p_binary = decimal_to_binary(p)
-    while compare_binary(s_temp, binary_multiplication(p_binary, [1, 0])):
-        k_temp = len(s_temp) // r
-
-        pad_with_zeros(s_temp, k_temp * r)
-
-        subvectors_temp = split_into_subvectors(s_temp, r)
-
-        s_temp = []
-
-        for i in range(0, k_temp):
-            subv = subvectors_temp[i]
-            s_temp = binary_addition(s_temp, binary_multiplication(subv, pow(2, r*(i-1)) % p))
-
-
-    if not compare_binary(p, s_temp):
-        s = s_temp - p
-    else:
-        s = s_temp
+    x = 7
+    p = 3
+    print("x = ", x)
+    bits_x = decimal_to_binary(x)
+    print("Zamiana na binarny x: ", bits_x)
+    print("p = ", p)
+    bits_p = decimal_to_binary(p)
+    print("Zamiana na binarny p: ", bits_p)
+    modulo_xp = modulo_function(bits_x, bits_p)
+    print("Wynik modulo: ", modulo_xp)
+    print("X = ", binary_to_decimal(bits_x))
+    print("P = ", binary_to_decimal(bits_p))
+    #print("Wynik modulo: ", binary_to_decimal(modulo_xp))
+main()
